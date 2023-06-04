07a9b8d12692124eb2c60922eeb5b5b1a6fa1300
brunobiasi <brunobiasi32@gmail.com>
2023-06-03 23:47:12 -0300

    iniciado o projeto

---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization{
+    public $id = 1;
+    public $name = 'Bruno Biasi';
+    public $site = 'https://github.com/brunobiasi';
+    public $description = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima vel ducimus dolores tenetur inventore consectetur minus placeat, sequi alias atque sed libero perspiciatis? Repellendus, accusantium harum eum nam nobis aliquid.';
+    
+}
